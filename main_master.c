87afe182f2c41f6bcadd77d4ae6a65d8216d3b87
lourdesvruiz <88477070+lourdesvruiz@users.noreply.github.com>
2022-03-01 00:39:00 -0600

    Update main_master.c

--- main_master.c
+++ main_master.c
@@ -1,14 +1,14 @@
 /*
- * File:   main.c
- * Author: Pablo
- * Ejemplo de uso de I2C Master
- * Created on 17 de febrero de 2020, 10:32 AM
+ * File:   micro proyecto 2.c
+ * Author: Lourdes Ruiz
+ * Creado:25/02/2022
+ * Descripcion: comunicacion I2C
  */
 //*****************************************************************************
-// Palabra de configuraci髇
+// Palabra de configuraci贸n
 //*****************************************************************************
 // CONFIG1
-#pragma config FOSC = EXTRC_NOCLKOUT// Oscillator Selection bits (RCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, RC on RA7/OSC1/CLKIN)
+#pragma config FOSC = INTRC_NOCLKOUT// Oscillator Selection bits (RCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, RC on RA7/OSC1/CLKIN)
 #pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
 #pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
 #pragma config MCLRE = OFF      // RE3/MCLR pin function select bit (RE3/MCLR pin function is digital input, MCLR internally tied to VDD)
@@ -27,101 +27,165 @@
 // Use project enums instead of #define for ON and OFF.
 
 //*****************************************************************************
-// Definici髇 e importaci髇 de librer韆s
+// Definici贸n e importaci贸n de librer铆as
 //*****************************************************************************
 #include <stdint.h>
 #include <pic16f887.h>
 #include "I2C.h"
 #include "LCD.h"
-#include "UART.h"
+#include "USART.h"
 #include <xc.h>
-//*****************************************************************************
-// Definici髇 de variables
-//*****************************************************************************
+//-----------------------------------------------------------------------------
+//---------------------------------| VARIABLES |------------------------------                    
+//-----------------------------------------------------------------------------
 #define _XTAL_FREQ 8000000
 
-//*****************************************************************************
-// Definici髇 de funciones para que se puedan colocar despu閟 del main de lo 
-// contrario hay que colocarlos todas las funciones antes del main
-//*****************************************************************************
+uint8_t contador = 0;
+char ESP32;
 
-/*
- * Variables 
- */
 
-uint8_t contador_main = 0;
-uint8_t centena, decena, unidad;
-uint8_t z;
-uint8_t dato;
-uint8_t semaforo;
+//-----------------------------------------------------------------------------
+//-------------------------| PROTOTIPO DE FUNCIONES |--------------------------                    
+//-----------------------------------------------------------------------------
+void config_ports(void);
+void config_oscilador(void);
+void config_I2C_MASTER(void);
 
-/*
- * Prototipos de funciones
- */
-void setup(void);
+//-----------------------------------------------------------------------------
+//---------------------------------| INTERRUPCION |----------------------------                   
+//-----------------------------------------------------------------------------
 
-//*****************************************************************************
-// Main
-//*****************************************************************************
-void main(void) {
-    setup();
-    
-    TRISD = 0x00;
-    Lcd_Clear();
-    Lcd_Set_Cursor(1,1);
-    Lcd_Write_String("S1:   S2:  Sem:");
-    
-    while(1){
-        /*
-        I2C_Master_Start();
-        I2C_Master_Write(0x50);
-        I2C_Master_Write(PORTB);
-        I2C_Master_Stop();
-        __delay_ms(200);
-        */
-        
-        //Lectura
-        I2C_Master_Start();
-        I2C_Master_Write(0x51);
-        contador_main = I2C_Master_Read(0);
-        I2C_Master_Stop();
-        //__delay_ms(200);
-        
-        Lcd_Set_Cursor(2,1);
-        
-        if (contador_main == 1){
-            Lcd_Write_String("V");
-        }
-        else if (contador_main == 2){
-            Lcd_Write_String("A");
-        }
-        else if (contador_main == 3){
-            Lcd_Write_String("R");
-        }
-        
-         
+
+void __interrupt() isr (void)
+{
+ 
+//                          Iinterrupci贸n del USART
+//----------------------------------------------------------------------------- 
+    if (PIR1bits.RCIF){       //Si se recibe algo (BUS DE REPECION ESTA LLENO)
+        ESP32 = RCREG;          
+        PIR1bits.RCIF = 0;    //Apagar la badera de interrupci贸n (cuando se lee)
+                              //el registro RCREG
     }
     return;
 }
-//*****************************************************************************
-// Funci髇 de Inicializaci髇
-//*****************************************************************************
-void setup(void){
-    ANSEL = 0;
-    ANSELH = 0;
-    TRISB = 0;
-    TRISD = 0;
+
+//-----------------------------------------------------------------------------
+//-------------------------------| LOOP PRINCIPAL |-----------------------------                    
+//-----------------------------------------------------------------------------
+
+void main() {
+    config_ports();
+    config_oscilador();
+    config_I2C_MASTER();
+    USART_init(_8MHz, _trON, _rcON);  //Config del modulo USART
+    config_interrupts();
+    
+    Lcd_Init();                         //inicializar la LCD
+    Lcd_Clear();                        //limpiar LCD
+    
+    Lcd_Set_Cursor(1,1);                //indicar la posicion
+    Lcd_Write_String("S1:      Sem:");
+
+    while(1){
+        
+        I2C_Master_Start();
+        I2C_Master_Write(0x50);
+        I2C_Master_Write(contador);
+        I2C_Master_Stop();
+        __delay_ms(200);
+        
+        /*
+        I2C_Master_Start();
+        I2C_Master_Write(0x51);
+        PORTD = I2C_Master_Read(0);
+        I2C_Master_Stop();
+        __delay_ms(200);
+         * */
+        Lcd_Set_Cursor(2,1);
+      
+        if (ESP32 == 1){
+            contador = 0;
+            Lcd_Write_String("           Verde");
+        }
+        else if (ESP32 == 2){
+            contador = 1;
+            Lcd_Write_String("        Amarillo");
+        }
+        else if (ESP32 == 3){
+            contador = 2;
+            Lcd_Write_String("            Rojo");
+        }
+        
+        /*
+        if(contador == 0){
+            
+            Lcd_Write_String("           Verde");
+            //__delay_ms(100);
+        }
+        
+        else if(contador == 1){
+            
+            Lcd_Write_String("        Amarillo");
+            PORTAbits.RA1 = 1;
+            PORTAbits.RA2 = 0;
+            //__delay_ms(100);
+        }
+        
+        else if(contador == 2){
+            Lcd_Write_String("            Rojo");
+            PORTAbits.RA1 = 0;
+            PORTAbits.RA2 = 1;
+            contador = 255;
+            //__delay_ms(100);
+        }
+        
+        __delay_ms(800);
+        contador++; 
+         * */  
+        PORTA = contador;
+        
+    }
+    
+    return;
+}
+
+//****************************************************************************
+//-------------------------------| FUNCIONES |-------------------------------                    
+//****************************************************************************
+
+//-----------------------------------------------------------------------------
+//                     CONFIG de entradas y salidas
+//-----------------------------------------------------------------------------    
+void config_ports(void)
+{
+    ANSEL = 0;  
+    ANSELH = 0;         //pines del puerto B como digitales
+    
+    TRISA = 0;                  //PORTA como salida     
+    TRISB = 0;                  //PORTD como salida
+    TRISD = 0;                  //PORTD como salida
+    //TRISE = 0;                  //PORTD como salida
+    
+    PORTA = 0;                  //Limpiar puertos
     PORTB = 0;
-    PORTD = 0;
-    
-     //Configuracion de oscilador
-    OSCCONbits.IRCF = 0b0111; //8MHz
-    OSCCONbits.SCS = 1; //ocsilador interno
-    
-    
-    USART_Init(8);
-    
-    Lcd_Init();
-    
-    I2C_Master_Init(100000);        // Inicializar Comuncaci髇 I2C
+    PORTD = 0;   
+    //PORTE = 0;
+}
+
+//-----------------------------------------------------------------------------
+//                      CONFIG oscilador 
+//-----------------------------------------------------------------------------
+void config_oscilador(void)
+{
+    OSCCONbits.IRCF = 0b0111;   //reloj a 8 MHz
+    OSCCONbits.SCS = 1;         //bit 0 en 1 para colocar el R.Interno
+}
+
+//-----------------------------------------------------------------------------
+//                      CONFIG SPI PARA EL MASTER 
+//-----------------------------------------------------------------------------
+void config_I2C_MASTER(void)
+{
+    I2C_Master_Init(100000);        // Inicializar Comuncaci贸n I2C
+
 }
